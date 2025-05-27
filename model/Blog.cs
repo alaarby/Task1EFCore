26a0b7227f11549392e6c520a29dbdd505ecf75c
alaarby <154479331+alaarby@users.noreply.github.com>
2025-05-27 13:42:35 +0300

    Add files via upload

---
+++ model/Blog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Model
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string description { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
